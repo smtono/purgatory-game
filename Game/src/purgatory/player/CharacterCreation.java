4538f08f05ca90190a7ae19ed485f94a6604fd06
smtono <64560893+smtono@users.noreply.github.com>
2020-09-04 01:14:35 -0500

    created CharacterCreation
    this is to start on plans of a character creation GUI that I will make so that we can set up the hero. this will be necessary to testing out the battle sequence later on.

---
+++ Game/src/purgatory/player/CharacterCreation.java
@@ -0,0 +1,9 @@
+package purgatory.player;
+/*
+    prompt for name
+    possibly make a quiz
+    otherwise, just let them pick what type of hero they want to be
+ */
+public class CharacterCreation {
+
+}
