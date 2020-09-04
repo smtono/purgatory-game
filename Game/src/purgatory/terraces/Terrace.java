0313d76b1c5f08ceeb4314e64753f8e45f6f16cb
smtono <64560893+smtono@users.noreply.github.com>
2020-09-04 00:04:32 -0500

    Created terrace enum
    these will represent each "level"

---
+++ Game/src/purgatory/terraces/Terrace.java
@@ -0,0 +1,62 @@
+package purgatory.terraces;
+
+import purgatory.entity.EntityType;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+    Author: Shannon Thornton
+
+   This will lay out the foundation of the "map" of the game. Purgatory is divided into seven terraces.
+   This is to represent the seven deadly sins.
+   Each terrace will have it's own enemy types and final boss.
+ */
+public enum Terrace {
+    // TODO: Fix variables for each
+    GLUTTONY("Gluttony", "", 1, 0, Arrays.asList()),
+    SLOTH("Sloth", "", 2, 0, Arrays.asList()),
+    AVARICE("Avarice", "", 3, 0, Arrays.asList()),
+    PRIDE("Pride", "", 4, 0, Arrays.asList()),
+    ENVY("Envy", "", 5, 0, Arrays.asList()),
+    LUST("Lust", "", 6, 0, Arrays.asList()),
+    WRATH("Wrath", "", 7, 0, Arrays.asList());
+
+    // variables for construction
+    private String name;
+    private String description;
+    private int level;
+    private int numRooms;
+    private List<EntityType> enemyTypes;
+
+    // CONSTRUCTOR
+    Terrace(String name,
+            String description,
+            int level,
+            int numRooms,
+            List<EntityType> enemyTypes) {
+        this.name = name;
+        this.description = description;
+        this.level = level;
+        this.numRooms = numRooms;
+        this.enemyTypes = enemyTypes;
+    }
+
+    // ACCESSORS
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public int getLevel() { return level; }
+    public int getNumRooms() { return numRooms; }
+    public List<EntityType> getEnemyTypes() { return enemyTypes; }
+
+    @Override
+    public String toString() {
+        return "Terrace{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", level=" + level +
+                ", numRooms=" + numRooms +
+                ", enemyTypes=" + enemyTypes +
+                '}';
+    }
+}
