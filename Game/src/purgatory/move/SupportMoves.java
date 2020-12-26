c68dd0651e7ff1e91a73d723367d67d04bb16737
smtono <64560893+smtono@users.noreply.github.com>
2020-12-25 20:36:42 -0600

    made SupportMoves.java implements Move.java

--- Game/src/purgatory/move/SupportMoves.java
+++ Game/src/purgatory/move/SupportMoves.java
@@ -4,5 +4,50 @@ package purgatory.move;
  *
  */
 public enum SupportMoves implements Move {
+    ;
 
+    @Override
+    public int useMana(int currMana) {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getResult() {
+        return 0;
+    }
+
+    @Override
+    public int getMana() {
+        return 0;
+    }
+
+    @Override
+    public double getAccuracy() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAffectAll() {
+        return false;
+    }
+
+    @Override
+    public int getLevelOfAccess() {
+        return 0;
+    }
+
+    @Override
+    public MoveType getMoveType() {
+        return null;
+    }
+
+    @Override
+    public AttackType getAttackType() {
+        return null;
+    }
 }
