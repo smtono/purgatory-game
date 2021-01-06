695b0c45c5220d816acfa15019ffc98cc72db629
smtono <64560893+smtono@users.noreply.github.com>
2021-01-06 00:22:23 -0600

    Start on SupportMove.java

---
+++ Game/src/purgatory/move/SupportMove.java
@@ -0,0 +1,25 @@
+package purgatory.move;
+
+import purgatory.weapon.AttackType;
+
+/**
+ *
+ */
+public enum SupportMove implements VirtualMove {
+    ;
+    
+    // ATTRIBUTES
+    private final Move move;
+    private final MoveType moveType = MoveType.SUPPORT;
+    private final AttackType attackType = AttackType.NONE;
+
+    // CONSTRUCTOR
+    SupportMove(String name, int result, int mana, double accuracy, boolean isAffectAll, int levelOfAccess) {
+        this.move = Move.of(name, result, mana, accuracy, isAffectAll, levelOfAccess, moveType, attackType);
+    }
+    
+    @Override
+    public Move getMove() {
+        return move;
+    }
+}
