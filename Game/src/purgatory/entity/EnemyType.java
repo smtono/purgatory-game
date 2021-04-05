aa740a28432cfd5a6162c7eb7976c37bc0ca3a77
smtono <64560893+smtono@users.noreply.github.com>
2021-04-04 23:31:55 -0500

    editing enemy types for new weapon types

--- Game/src/purgatory/entity/EnemyType.java
+++ Game/src/purgatory/entity/EnemyType.java
@@ -1,5 +1,6 @@
 package purgatory.entity;
 
+import purgatory.terraces.Terrace;
 import purgatory.weapon.WeaponType;
 
 import java.util.Arrays;
@@ -7,52 +8,103 @@ import java.util.List;
 
 public enum EnemyType implements VirtualEntityType {
     // angels
-    GUARDIAN("Guardian",
-            "A hearty foe, the lowest order of angels. They wield a sword and club.",
-            CharacterType.ENEMY,
-            Arrays.asList(WeaponType.WAND)),
+    GUARDIAN("Guardian", "A hearty foe, the lowest order of angels.", CharacterType.ENEMY, Arrays.asList(WeaponType.SWORD, WeaponType.CLUB), Terrace.GLUTTONY),
     ARCHANGEL("Archangel",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.SWORD)),
+            Arrays.asList(WeaponType.SWORD, WeaponType.SPEAR),
+            Terrace.SLOTH),
     PRINCIPALITY("Principality",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.STAFF)),
+            Arrays.asList(WeaponType.STAFF),
+            Terrace.AVARICE),
     POWER("Power",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.BOW, WeaponType.AXE)),
+            Arrays.asList(WeaponType.BOW, WeaponType.SCYTHE),
+            Terrace.PRIDE),
     VIRTUE("Virtue",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.TOME)),
+            Arrays.asList(WeaponType.TOME),
+            Terrace.LUST),
     DOMINATION("Domination",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.SWORD, WeaponType.CLUB)),
+            Arrays.asList(WeaponType.MACE, WeaponType.HAMMER),
+            Terrace.LUST),
     THRONE("Throne",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.WAND)),
+            Arrays.asList(WeaponType.WAND),
+            Terrace.WRATH),
     SERAPHIM("Seraphim",
             "",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.STAFF, WeaponType.TOME)),
+            Arrays.asList(WeaponType.STAFF, WeaponType.TOME),
+            Terrace.WRATH),
 
     // spheres
     MOON("Moon",
-            "The first sphere of Paradiso, a powerful elemental magic user.",
+            "The Inconstant, a powerful elemental magic user.",
             CharacterType.ENEMY,
-            Arrays.asList(WeaponType.WAND, WeaponType.STAFF));
+            Arrays.asList(WeaponType.WAND, WeaponType.STAFF),
+            Terrace.GLUTTONY),
+    MERCURY("Mercury",
+            "The Ambitious, a powerful elemental magic user.",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.WAND),
+            Terrace.SLOTH),
+    VENUS("Venus",
+            "The Lovers, ",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.RAPIER, WeaponType.DAGGER),
+            Terrace.AVARICE),
+    SUN("Sun",
+            "The Wise, ",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.TOME),
+            Terrace.PRIDE),
+    MARS("Mars",
+            "The Warrior of Faith, ",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.MACE, WeaponType.LANCE),
+            Terrace.ENVY),
+    JUPITER("Jupiter",
+            "The Just Ruler, ",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.STAFF, WeaponType.RAPIER),
+            Terrace.LUST),
+    SATURN("Saturn",
+            "The Contemplative, ",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.TOME, WeaponType.WAND),
+            Terrace.LUST),
+    STAR("Fixed Stars",
+            "",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.WAND),
+            Terrace.WRATH),
+    PRIMUM("Primum Mobile",
+            "",
+            CharacterType.ENEMY,
+            Arrays.asList(WeaponType.TOME, WeaponType.WAND, WeaponType.STAFF),
+            Terrace.WRATH),
+
+    ;
 
     //ATTRIBUTES
     private final EntityType entityType;
+    private final Terrace lowestTerrace; // the earliest level this enemy will be found at
 
     // CONSTRUCTOR
-    EnemyType(String typeName, String description, CharacterType type, List<WeaponType> weaponTypeTypes) {
+    EnemyType(String typeName, String description, CharacterType type, List<WeaponType> weaponTypeTypes, Terrace lowestTerrace) {
         this.entityType = EntityType.of(typeName, description, type, weaponTypeTypes);
+        this.lowestTerrace = lowestTerrace;
     }
+
+    public Terrace getLowestTerrace() { return lowestTerrace; }
 
     @Override
     public EntityType getEntityType() { return entityType; }
