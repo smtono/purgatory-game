12a93de7f9e49b914e5a5bbe294333bfb05898f5
smtono <64560893+smtono@users.noreply.github.com>
2021-05-08 11:38:51 -0500

    update morality to keep track of morality

--- Game/src/purgatory/story/Morality.java
+++ Game/src/purgatory/story/Morality.java
@@ -4,14 +4,28 @@ package purgatory.story;
  *
  */
 public class Morality {
-    public static int setMorality(int morality, Choice choice) {
-        switch (choice) {
+    // ATTRIBUTES
+    private static int morality;
+
+    // CONSTRUCTOR
+    public Morality(int morality) {
+        Morality.morality = morality;
+    }
+
+    // ACCESSOR/MUTATOR
+    public int getMorality() { return morality; }
+
+    /** Takes in a choice and adds morality to it depending on the choice type */
+    public static void setMorality(ChoiceType choiceType) {
+        switch (choiceType) {
             case GOOD:
-                return morality += 1;
+                morality += 1;
+                break;
             case EVIL:
-                return  morality -= 1;
-            default: // "neutral" option
-                return morality;
+                morality -= 1;
+                break;
+            default: // nothing happens
+                break;
         }
     }
 
