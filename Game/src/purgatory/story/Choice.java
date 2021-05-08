15b41a9f44825f3aa97978aa70b07796ddd07743
smtono <64560893+smtono@users.noreply.github.com>
2021-05-08 11:38:02 -0500

    create story dialog

--- Game/src/purgatory/story/Choice.java
+++ Game/src/purgatory/story/Choice.java
@@ -1,5 +1,20 @@
 package purgatory.story;
 
-public enum Choice {
-    GOOD, EVIL;
+/**
+ * Choice is used to make dialogue options for StoryDialog
+ */
+public class Choice {
+    // ATTRIBUTES
+    private final ChoiceType choiceType;
+    private final String dialogue;
+
+    // CONSTRUCTOR
+    public Choice(ChoiceType choiceType, String dialogue) {
+        this.choiceType = choiceType;
+        this.dialogue = dialogue;
+    }
+
+    // ACCESSORS
+    public ChoiceType getChoiceType() { return choiceType; }
+    public String getDialogue() { return dialogue; }
 }
---
+++ Game/src/purgatory/story/ChoiceType.java
@@ -0,0 +1,25 @@
+package purgatory.story;
+
+/**
+ * ChoiceType is an enum for different types of dialogue choices in the story
+ */
+public enum ChoiceType {
+    // ENUM CHOICES
+    GOOD("Good", "You feel this is the right choice."),
+    EVIL("Evil", "You feel that wasn't right..."),
+    NEUTRAL("Neutral", "You feel that had no effect.");
+
+    // ATTRIBUTES
+    private final String choice;
+    private final String description;
+
+    // CONSTRUCTOR
+    ChoiceType(String choice, String description) {
+        this.choice = choice;
+        this.description = description;
+    }
+
+    // ACCESSORS
+    public String getChoice() { return choice; }
+    public String getDescription() { return description; }
+}
