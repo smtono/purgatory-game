42ddd9f9b30e771113d94012fd20e9023975fdaf
smtono <64560893+smtono@users.noreply.github.com>
2021-05-12 16:13:42 -0500

    added new hero weapons (for new hero types))

---
+++ Game/src/purgatory/weapon/magic/Fan.java
@@ -0,0 +1,54 @@
+package purgatory.weapon.magic;
+
+import purgatory.move.Move;
+import purgatory.move.type.Attack;
+import purgatory.move.type.MoveType;
+import purgatory.move.type.Support;
+import purgatory.weapon.AttackType;
+import purgatory.weapon.ManaType;
+
+public enum Fan implements Attack, Support {
+    ;
+
+    private final Move move;
+    private final MoveType moveType = MoveType.ATTACK;
+    private final AttackType attackType = AttackType.PIERCE;
+
+    //  CONSTRUCTOR
+    Fan(String name, int result, int mana, double accuracy, boolean isAffectAll, int levelOfAccess) {
+        this.move = Move.of(name, result, mana, accuracy, isAffectAll, levelOfAccess, moveType, attackType);
+    }
+
+    // IMPLEMENTED METHODS
+    @Override
+    public Move getMove() { return move; }
+
+    @Override
+    public boolean isStrength() { return true; }
+
+    @Override
+    public boolean isMagic() {
+        return false;
+    }
+
+    @Override
+    public ManaType getManaType() { return ManaType.STRENGTH; }
+
+    @Override
+    public String toString() {
+        String affectAll;
+
+        if (isAffectAll()) {
+            affectAll = "Yes";
+        }
+        else {
+            affectAll = "No";
+        }
+
+        return "Name: " + getName() +
+                "\nDamage: " + getResult() +
+                "\nMana: " + getMana() +
+                "\nAccuracy: " + getAccuracy() +
+                "\nAffects all?: " + affectAll;
+    }
+}
