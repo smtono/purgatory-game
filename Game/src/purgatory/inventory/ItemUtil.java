61bad70ccd52495c1619b44fb7e9cd923488b78c
smtono <64560893+smtono@users.noreply.github.com>
2021-05-13 17:37:42 -0500

    added method to find item by name

--- Game/src/purgatory/inventory/ItemUtil.java
+++ Game/src/purgatory/inventory/ItemUtil.java
@@ -1,4 +1,22 @@
 package purgatory.inventory;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ItemUtil {
+    /**
+     *
+     * @param itemName
+     * @return
+     */
+    public static Item findItemOfName(String itemName) {
+        List<Item> items = Arrays.asList(Item.values().clone());
+
+        for (Item item : items) {
+            if (item.getName().equals(itemName)) {
+                return item;
+            }
+        }
+        return Item.NONE;
+    }
 }
