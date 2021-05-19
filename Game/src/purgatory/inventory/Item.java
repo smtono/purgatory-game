5819621d89f280c169bec615199daad37c79d379
smtono <64560893+smtono@users.noreply.github.com>
2021-05-19 17:01:54 -0500

    made new items

--- Game/src/purgatory/inventory/Item.java
+++ Game/src/purgatory/inventory/Item.java
@@ -5,21 +5,29 @@ package purgatory.inventory;
  */
 public enum Item {
     NONE("null", "", null, 0),
+
     // HEALING
     BANDAGE("Bandage", "Provides 10 HP", ItemType.HEAL, 10),
     OINTMENT("Ointment", "Provides 50 HP", ItemType.HEAL, 50),
     MEDICINE("Medicine", "Provides 100 HP", ItemType.HEAL, 100),
-    HERBS("Herbs", "Provides 200 HP", ItemType.HEAL, 200),
+    VULNERARY("Vulnerary", "Provides 200 HP", ItemType.HEAL, 200),
     PRAYER("Prayer", "Provides 500 HP", ItemType.HEAL, 500),
 
     // MANA
-    DRINK("Energy Drink", "Provides 5 MP", ItemType.MANA, 5),
+    DRINK("Energy Drink", "Provides 10 MP", ItemType.MANA, 10),
+    COFFEE("Coffee", "Provides 25 MP", ItemType.MANA, 25),
+    SWEETS("Sweets", "Provides 50 MP", ItemType.MANA, 50),
+    ORANGE("Orange Juice", "Provides 80 MP", ItemType.MANA, 80),
+    KISS("Kiss", "Provides 100 MP", ItemType.MANA, 100),
 
     // BUFF
+    
 
     // DEBUFF
 
+    
     // ATTACKS
+    
     ;
 
     // ATTRIBUTES
